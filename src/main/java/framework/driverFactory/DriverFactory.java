f77b936709a7b8ec19f20c71b119295d212ae8a2
olegdatsiuk <oleg.datsiuk@getoxygen.com>
2022-05-13 20:15:57 +0300

    added chrome and firefox and smoke suite

--- src/main/java/framework/driverFactory/DriverFactory.java
+++ src/main/java/framework/driverFactory/DriverFactory.java
@@ -3,14 +3,24 @@ package framework.driverFactory;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class DriverFactory {
 
     private static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
 
     public static void setUp() {
-        WebDriverManager.chromedriver().setup();
-        WebDriver webDriver = new ChromeDriver();
+        WebDriver webDriver;
+        String browser = System.getProperty("BROWSER");
+        if (browser.equals("chrome")) {
+            WebDriverManager.chromedriver().setup();
+            webDriver = new ChromeDriver();
+        } else if (browser.equals("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            webDriver = new FirefoxDriver();
+        } else {
+            throw new IllegalStateException("Please, pass chrome or firefox");
+        }
         webDriver.manage().window().maximize();
         driver.set(webDriver);
     }
